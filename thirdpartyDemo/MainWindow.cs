4db4b3a37d5a7301d2a2d517ed6ea9499c389455
Bilahari <bilahari@bilaharis-mbp.logitech.com>
2017-01-10 16:52:28 -0800

    first commit

---
+++ thirdpartyDemo/MainWindow.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Gtk;
+using System.Threading;
+using System.Text;
+
+
+using NetMQ;
+using NetMQ.Sockets;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+//using ServiceStack.Redis;
+//using ServiceStack.Text;
+
+public partial class MainWindow : Gtk.Window
+{
+	ResponseSocket responseSocket;
+	RequestSocket requestSocket;
+
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		responseSocket = new ResponseSocket("tcp://*:5556");
+		requestSocket = new RequestSocket("tcp://localhost:5555");
+			
+		BackgroundWorker bw = new BackgroundWorker();
+
+		bw.WorkerReportsProgress = true;
+
+		bw.DoWork += delegate (object o, DoWorkEventArgs args1)
+		          {
+			          Console.WriteLine("Listening for messages on 5556");
+
+					  while (true)
+				    {
+				         var message = responseSocket.ReceiveFrameString();
+
+				     //  var message = responseSocket.ReceiveFrameBytes();
+
+					    //object p =  JsonConvert.DeserializeObject(Encoding.UTF8.GetString(message));
+
+						Console.WriteLine("Response socket received:" + message);
+
+				        this.hscale3.Value = Double.Parse(message);
+
+						Console.WriteLine("Response sending back Ack for :" + message);
+					    
+						responseSocket.SendFrame("Done setting to value " + message);
+ 				     }
+			      };
+
+		bw.RunWorkerAsync();
+			
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void OnHscale3MoveSlider(object o, MoveSliderArgs args)
+	{
+		Console.WriteLine(args.ToString());
+	}
+
+	protected void OnHscale3ValueChanged(object sender, EventArgs e)
+	{
+		HScale hs = (HScale)sender;
+		//Console.WriteLine(hs.Value);
+
+	
+		Console.WriteLine("requestSocket : Sending " + hs.Value.ToString());
+		requestSocket.SendFrame(hs.Value.ToString());
+
+		var message = requestSocket.ReceiveFrameString();
+		Console.WriteLine("requestSocket : Received " + message);
+		
+
+		//const string ChannelName = "SimplePubSubCHANNEL";
+		//var redisPublisher = new RedisClient();
+		//ThreadPool.QueueUserWorkItem(x =>
+		//		{
+		//			//Thread.Sleep(200);
+		//			//Console.WriteLine("Publishing '{0}' to '{1}'", hs.Value, ChannelName);
+		//			//redisPublisher.PublishMessage(ChannelName, hs.Value.ToString());
+		//});
+	}
+}
+
---
+++ thirdpartyDemo/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Gtk;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+using System.Threading;
+
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace thirdpartyDemo
+{
+
+	class MainClass
+	{
+
+		public static void Main(string[] args)
+		{
+			Application.Init();
+
+			MainWindow win = new MainWindow();
+
+			win.ShowAll();
+
+			//using (var responseSocket = new ResponseSocket("tcp://*:5555"))
+			//using (var requestSocket = new RequestSocket("tcp://localhost:5555"))
+			//{
+			//	//while (true)
+			//	{
+			//		Console.WriteLine("requestSocket : Sending ");
+			//		requestSocket.SendFrame("hello");
+
+			//		var message = requestSocket.ReceiveFrameString();
+			//		Console.WriteLine("responseSocket : Server Received '{0}'", message);
+			//	}
+			//}
+			//const string ChannelName = "SimplePubSubCHANNEL";
+			//const string MessagePrefix = "MESSAGE ";
+			//const int PublishMessageCount = 5;
+
+			//int messagesReceived = 0;
+
+			//using (var redisConsumer = new RedisClient())
+			//using (var subscription = redisConsumer.CreateSubscription())
+			//{
+			//	subscription.OnSubscribe = channel =>
+			//	{
+			//		Console.WriteLine("Subscribed to '{0}'", channel);
+			//	};
+			//	subscription.OnUnSubscribe = channel =>
+			//	{
+			//		Console.WriteLine("UnSubscribed from '{0}'", channel);
+			//	};
+			//	subscription.OnMessage = (channel, msg) =>
+			//	{
+			//		Console.WriteLine("Received '{0}' from channel '{1}'", msg, channel);
+
+			//			//As soon as we've received all 5 messages, disconnect by unsubscribing to all channels
+			//			if (++messagesReceived == PublishMessageCount)
+			//		{
+			//			subscription.UnSubscribeFromAllChannels();
+			//		}
+			//	};
+
+				//ThreadPool.QueueUserWorkItem(x =>
+				//{
+				//	Thread.Sleep(200);
+				//	Console.WriteLine("Begin publishing messages...");
+
+				//	using (var redisPublisher = new RedisClient())
+				//	{
+				//		for (var i = 1; i <= PublishMessageCount; i++)
+				//		{
+				//			var message = MessagePrefix + i;
+				//			Console.WriteLine("Publishing '{0}' to '{1}'", message, ChannelName);
+				//			redisPublisher.PublishMessage(ChannelName, message);
+				//		}
+				//	}
+				//});
+
+				//Console.WriteLine("Started Listening On '{0}'", ChannelName);
+				//subscription.SubscribeToChannels(ChannelName); //blocking
+			//}
+			Application.Run();
+		}
+	}
+}
